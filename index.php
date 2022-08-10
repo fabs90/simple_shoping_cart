a2f5aab703ee961e1b2f9abf2d19cd45cc03fbe7
Fabs90 <fabianjuliansyah89@gmail.com>
2022-08-11 00:30:47 +0700

    Add array session and will be use in cart

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 require 'connection.php';
+session_start();
 ?>
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Ultramarket</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
   </head>
@@ -14,7 +15,7 @@ require 'connection.php';
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark shadow-sm" style="background-color:#e9967a">
       <div class="container">
-        <a class="navbar-brand" href="#">Ultramarket</a>
+        <a class="navbar-brand" href="index.php">Ultramarket</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -95,3 +96,35 @@ while ($row = mysqli_fetch_array($result)) {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
+<?php
+if (isset($_POST["kirim"])) {
+    // Ambil data dari inputan user
+    $id = $_GET["id"];
+    $nama = $_POST["nama"];
+    $harga = number_format($_POST["harga"], 2);
+    $jumlah_barang = $_POST["jumlah_barang"];
+
+    // Kalau blom ada array session_cart, buat
+    if (!isset($_SESSION['cart'])) {
+        $_SESSION['cart'][] = array(
+            'id' => $id,
+            'nama' => $nama,
+            'harga' => $harga,
+            'jumlah_barang' => $jumlah_barang,
+        );
+    } else {
+        // Ambil semua ID saja dari array session_cart
+        $SESSION_CART_ID = array_column($_SESSION['cart'], 'id');
+        // Kalo id nya gaada di array, masukin ke array
+        if (!in_array($id, $SESSION_CART_ID)) {
+            $_SESSION['cart'][] = array(
+                'id' => $id,
+                'nama' => $nama,
+                'harga' => $harga,
+                'jumlah_barang' => $jumlah_barang,
+            );
+        }
+    }
+
+}
+?>
